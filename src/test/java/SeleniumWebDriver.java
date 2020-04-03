f6456836aff528e5750b09ef8243b3c9bb7d8a09
pedroapple24 <rlpedro92@gmail.com>
2020-04-03 17:18:01 -0600

    adding selenium test

--- src/test/java/SeleniumWebDriver.java
+++ src/test/java/SeleniumWebDriver.java
@@ -1,10 +1,13 @@
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.*;
 import org.junit.Test;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 
 import java.util.List;
 
 public class SeleniumWebDriver {
+
 
     @Test
     public void testGoogleSearch() throws InterruptedException {
@@ -75,15 +78,91 @@ public class SeleniumWebDriver {
         driver.findElement(By.name("passwd")).sendKeys("abcdefghlkjl");
         driver.findElement(By.id("SubmitLogin")).submit();
         System.out.println("Login Done with Submit");
-
         //driver.close();
-
     }
 
+    @Test
+    public void checkBox(){
 
+        System.setProperty("webdriver.chrome.driver", "/Users/pedro/Documents/sw_test/chromedriver");
+        WebDriver driver = new ChromeDriver();
 
+        driver.get("http://demo.guru99.com/test/radio.html");
+        WebElement radio1 = driver.findElement(By.id("vfb-7-1"));
+        WebElement radio2 = driver.findElement(By.id("vfb-7-2"));
 
+        //Radio Button1 is selected
+        radio1.click();
+        System.out.println("Radio Button Option 1 Selected");
 
+        //Radio Button1 is de-selected and Radio Button2 is selected
+        radio2.click();
+        System.out.println("Radio Button Option 2 Selected");
+
+        // Selecting CheckBox
+        WebElement option1 = driver.findElement(By.id("vfb-6-0"));
+
+        // This will Toggle the Check box
+        option1.click();
+
+        // Check whether the Check box is toggled on
+        if (option1.isSelected()) {
+            System.out.println("Checkbox is Toggled On");
+
+        } else {
+            System.out.println("Checkbox is Toggled Off");
+        }
+
+        //Selecting Checkbox and using isSelected Method
+        driver.get("http://demo.guru99.com/test/facebook.html");
+        WebElement chkFBPersist = driver.findElement(By.id("persist_box"));
+        for (int i=0; i<2; i++) {
+            chkFBPersist.click ();
+            System.out.println("Facebook Persists Checkbox Status is -  "+chkFBPersist.isSelected());
+        }
+        //driver.close();
+    }
+
+    @Test
+    public void clickOnImage(){
+
+        String baseUrl = "https://www.facebook.com/login/identify?ctx=recover";
+        System.setProperty("webdriver.chrome.driver", "/Users/pedro/Documents/sw_test/chromedriver");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get(baseUrl);
+        //click on the "Facebook" logo on the upper left portion
+        driver.findElement(By.cssSelector("a[title=\"Ir a la página de inicio de Facebook\"]")).click();
+        //<a href="https://www.facebook.com/" title="Ir a la página de inicio de Facebook"><i class="fb_logo img sp_lMWDbGC4nGf_2x sx_3bfd4e"><u>Facebook</u></i></a>
+
+        //verify that we are now back on Facebook's homepage
+        if (driver.getTitle().equals("Facebook - Inicia sesión o regístrate")) {
+
+            //<title id="pageTitle">Facebook - Inicia sesión o regístrate</title>
+            System.out.println("We are back at Facebook's homepage");
+        } else {
+            System.out.println("We are NOT in Facebook's homepage");
+        }
+        driver.close();
+    }
+
+    @Test
+    public void acessDrop(){
+
+        System.setProperty("webdriver.gecko.driver","/Users/pedro/Documents/sw_test/geckodriver");
+        String baseURL = "http://demo.guru99.com/test/newtours/register.php";
+        WebDriver driver = new FirefoxDriver();
+        driver.get(baseURL);
+
+        Select drpCountry = new Select(driver.findElement(By.name("country")));
+        drpCountry.selectByVisibleText("ANTARCTICA");
+
+        //Selecting Items in a Multiple SELECT elements
+        driver.get("http://jsbin.com/osebed/2");
+        Select fruits = new Select(driver.findElement(By.id("fruits")));
+        fruits.selectByVisibleText("Banana");
+        fruits.selectByIndex(1);
+
+    }
 }
-
 
