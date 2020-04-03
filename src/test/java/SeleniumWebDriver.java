724f6236163fa324a042380e82c051b826a70728
pedroapple24 <rlpedro92@gmail.com>
2020-04-03 08:28:18 -0600

    adding selenium test

---
+++ src/test/java/SeleniumWebDriver.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+import org.junit.Test;
+
+public class SeleniumWebDriver {
+
+    @Test
+    public void testGoogleSearch() throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver", "/Users/pedro/Documents/sw_test/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://www.google.com/");
+        Thread.sleep(5000);  // Let the user actually see something!
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys("ChromeDriver");
+        searchBox.submit();
+        Thread.sleep(5000);  // Let the user actually see something!
+        driver.quit();
+    }
+
+
+
+}
+
+
