1e028a658371515114aae4bd617ab1c5ba17810b
pedroapple24 <rlpedro92@gmail.com>
2020-04-05 14:42:10 -0500

    More Selenium test

--- src/test/java/SeleniumWebDriver.java
+++ src/test/java/SeleniumWebDriver.java
@@ -6,9 +6,14 @@ import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 public class SeleniumWebDriver {
 
@@ -330,5 +335,92 @@ public class SeleniumWebDriver {
         driver.quit();
     }
 
+    @Test
+    public void rowTable() {
+        WebDriver wd;
+        System.setProperty("webdriver.chrome.driver", "/Users/pedro/Documents/sw_test/chromedriver");
+        wd = new ChromeDriver();
+        wd.get("http://demo.guru99.com/test/web-table-element.php");
+        wd.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        WebElement baseTable = wd.findElement(By.tagName("table"));
+
+        //To find third row of table
+        WebElement tableRow = baseTable.findElement(By.xpath("//*[@id=\"leftcontainer\"]/table/tbody/tr[3]"));
+        String rowtext = tableRow.getText();
+        System.out.println("Third row of table : " + rowtext);
+
+        //to get 3rd row's 2nd column data
+        WebElement cellIneed = tableRow.findElement(By.xpath("//*[@id=\"leftcontainer\"]/table/tbody/tr[3]/td[2]"));
+        String valueIneed = cellIneed.getText();
+        System.out.println("Cell value is : " + valueIneed);
+        wd.close();
+    }
+
+    @Test
+    public void findBrokenLinks(){
+
+        WebDriver driver = null;
+        System.setProperty("webdriver.chrome.driver", "/Users/pedro/Documents/sw_test/chromedriver");
+
+        // TODO Auto-generated method stub
+
+            String homePage = "http://www.zlti.com";
+            String url = "";
+            HttpURLConnection huc = null;
+            int respCode = 200;
+
+            driver = new ChromeDriver();
+
+            driver.manage().window().maximize();
+
+            driver.get(homePage);
+
+            List<WebElement> links = driver.findElements(By.tagName("a"));
+
+            Iterator<WebElement> it = links.iterator();
+
+            while(it.hasNext()){
+
+                url = it.next().getAttribute("href");
+
+                System.out.println(url);
+
+                if(url == null || url.isEmpty()){
+                    System.out.println("URL is either not configured for anchor tag or it is empty");
+                    continue;
+                }
+
+                if(!url.startsWith(homePage)){
+                    System.out.println("URL belongs to another domain, skipping it.");
+                    continue;
+                }
+
+                try {
+                    huc = (HttpURLConnection)(new URL(url).openConnection());
+
+                    huc.setRequestMethod("HEAD");
+
+                    huc.connect();
+
+                    respCode = huc.getResponseCode();
+
+                    if(respCode >= 400){
+                        System.out.println(url+" is a broken link");
+                    }
+                    else{
+                        System.out.println(url+" is a valid link");
+                    }
+
+                } catch (MalformedURLException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+
+            driver.quit();
+    }
 }
 
