aeda012a950f01c62c48af3a946d84db137ece3d
pedroapple24 <rlpedro92@gmail.com>
2020-04-04 20:28:01 -0600

    Adding more test for selenium :)

--- src/test/java/SeleniumWebDriver.java
+++ src/test/java/SeleniumWebDriver.java
@@ -6,7 +6,9 @@ import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 public class SeleniumWebDriver {
 
@@ -168,7 +170,7 @@ public class SeleniumWebDriver {
     @Test
     public void byList(){
         String baseUrl = "http://demo.guru99.com/test/link.html";
-        System.setProperty("webdriver.gecko.driver","/Users/pedro/Documents/sw_test/geckodriver");
+        System.setProperty("webdriver.chrome.driver", "/Users/pedro/Documents/sw_test/chromedriver");
         WebDriver driver = new ChromeDriver();
         driver.get(baseUrl);
         driver.findElement(By.linkText("click here")).click();
@@ -246,6 +248,86 @@ public class SeleniumWebDriver {
 
         // click the "UploadFile" button
         driver.findElement(By.name("send")).click();
+    }
+
+    @Test
+    public void Alert() throws NoAlertPresentException,InterruptedException{
+
+        System.setProperty("webdriver.chrome.driver", "/Users/pedro/Documents/sw_test/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        // Alert Message handling
+
+        driver.get("http://demo.guru99.com/test/delete_customer.php");
+
+        driver.findElement(By.name("cusid")).sendKeys("53920");
+        driver.findElement(By.name("submit")).submit();
+
+        // Switching to Alert
+        Alert alert = driver.switchTo().alert();
+
+        // Capturing alert message.
+        String alertMessage= driver.switchTo().alert().getText();
+
+        // Displaying alert message
+        System.out.println(alertMessage);
+        Thread.sleep(5000);
+
+        // Accepting alert
+        alert.accept();
+    }
+
+    @Test
+    public void windowHandle(){
+
+        System.setProperty("webdriver.gecko.driver","/Users/pedro/Documents/sw_test/geckodriver");
+        WebDriver driver=new FirefoxDriver();
+
+        //Launching the site.
+        driver.get("http://demo.guru99.com/popup.php");
+        driver.manage().window().maximize();
+
+        driver.findElement(By.xpath("//*[contains(@href,'popup.php')]")).click();
+
+        String MainWindow=driver.getWindowHandle();
+
+        // To handle all new opened window.
+        Set<String> s1=driver.getWindowHandles();
+        Iterator<String> i1=s1.iterator();
+
+        while(i1.hasNext())
+        {
+            String ChildWindow=i1.next();
+
+            if(!MainWindow.equalsIgnoreCase(ChildWindow))
+            {
+
+                // Switching to Child window
+                driver.switchTo().window(ChildWindow);
+                driver.findElement(By.name("emailid"))
+                        .sendKeys("gaurav.3n@gmail.com ");
+
+                                driver.findElement(By.name("btnLogin")).click();
+
+                // Closing the Child Window.
+                driver.close();
+            }
+        }
+        // Switching to Parent window i.e Main Window.
+        driver.switchTo().window(MainWindow);
+    }
+
+    @Test
+    public void webHandle(){
+
+        System.setProperty("webdriver.gecko.driver","/Users/pedro/Documents/sw_test/geckodriver");
+        String baseUrl = "http://demo.guru99.com/test/write-xpath-table.html";
+        WebDriver driver = new FirefoxDriver();
+
+        driver.get(baseUrl);
+        String innerText = driver.findElement(
+                By.xpath("//table/tbody/tr[2]/td[2]")).getText();
+        System.out.println(innerText);
+        driver.quit();
     }
 
 }
